e921abf8eb37284059f9c718ef6914fb9d2816f2
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2019-11-19 21:01:21 +0100

    Profile index + middleware
    User can now see his credentials on his profile page. Only logged in users can go to /profile using middleware.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,18 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+use Request;
 
 class ProfileController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
-        return view('profile.index');
+        $userId = Auth::user()->id;
+        $user = User::findOrFail($userId);
+
+        return view('profile.index', [
+            'user' => $user,
+        ]);
     }
 
     public function edit()
     {
-
+        
     }
 
     public function update()
@@ -21,3 +33,4 @@ class ProfileController extends Controller
 
     }
 }
+    
