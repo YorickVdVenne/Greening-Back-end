cc776ad1e7efabe4e1cfb414b23cca7b650b0fdd
Vaannelies <0962949@hr.nl>
2019-11-19 13:17:55 +0100

    Test push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Greening
+                    Greening!
                 </div>
             </div>
         </div>
