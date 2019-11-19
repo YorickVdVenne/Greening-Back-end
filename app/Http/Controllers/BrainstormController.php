b5ffa75f7e17ee3bd57880933e62c763a69d99e1
Vaannelies <0962949@hr.nl>
2019-11-19 15:29:35 +0100

    added functions to controllers
    and created ProfileController

--- app/Http/Controllers/BrainstormController.php
+++ app/Http/Controllers/BrainstormController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class BrainstormController extends Controller
 {
-    //
+    public function index()
+    {
+        
+    }
 }
--- app/Http/Controllers/TipsAndTricksController.php
+++ app/Http/Controllers/TipsAndTricksController.php
@@ -6,5 +6,39 @@ use Illuminate\Http\Request;
 
 class TipsAndTricksController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
 }
