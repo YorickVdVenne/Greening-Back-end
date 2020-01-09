5beeee204d53f5ff05689f58a8a643009eec3e04
Vaannelies <0962949@hr.nl>
2020-01-09 15:20:13 +0100

    self column has working content in seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,16 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('ideas')->insert([
-            'user_id' => 1,
-            'title' => "Aapjes",
-            'subject' => "Dieren",
-            'description' => "apen zijn cool 4ever"
+            'id'            => 32,
+            'user_id'       => 1,
+            'title'         => "Aapjes",
+            'subject'       => "Dieren",
+            'description'   => "apen zijn cool 4ever",
+            'self'          => "http://greening.louis.lol/api/ideas/32"
         ]);
         DB::table('ideas')->insert([
-            'user_id' => 1,
-            'title' => "Auto",
-            'subject' => "Transport",
-            'description' => "pak de fiets hee"
+            'id'            => 33,
+            'user_id'       => 1,
+            'title'         => "Auto",
+            'subject'       => "Transport",
+            'description'   => "pak de fiets hee",
+            'self'          => "http://greening.louis.lol/api/ideas/33"
         ]);
     }
 }
